3f921768a746c4b24719b8649226f0fac6aa3306
Aryan Tajne <atajne@umd.edu>
2024-04-20 21:58:40 +0000

    maybe I AM a data analyst

---
+++ data_scraping/filteringscripts/finalcourselistplanetterp.py
@@ -0,0 +1,34 @@
+import json
+
+def read_from_json(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
+
+def write_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def get_intersection_and_difference(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    intersection = set1 & set2
+    difference1 = set1 - set2
+    difference2 = set2 - set1
+    return list(intersection), list(difference1), list(difference2)
+
+filename = r'/home/atajne/terpilot/data_scraping/intermediatedata/unique_courses_planetterp.json'
+data = read_from_json(filename)
+
+grading_courses = data['grading_courses']
+info_courses = data['info_courses']
+
+intersection, removed_from_grading, removed_from_info = get_intersection_and_difference(grading_courses, info_courses)
+
+print("Intersection: ", intersection)
+print("Removed from grading_courses: ", removed_from_grading)
+print("Removed from info_courses: ", removed_from_info)
+
+# Write the intersection back to the file
+data['final_list'] = intersection
+write_to_json(data, filename)
---
+++ data_scraping/filteringscripts/finaltestudodata.py
@@ -0,0 +1,71 @@
+import os
+import json
+
+def read_from_json(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
+
+def write_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def process_data(directory, courses, professors):
+    processed = 0
+    removed = 0
+    removed_section = 0
+    written = 0
+    removed_courses = []
+    removed_courses_due_to_missing_section = []
+    final_data = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            with open(os.path.join(directory, filename), 'r') as f:
+                print(f"Processing {filename}")
+                data = json.load(f)
+                for item in data:
+                    processed += 1
+                    print(f"    Processing {item['course_number']}")
+                    if item['course_number'] not in courses:
+                        removed_courses.append(item['course_number'])
+                        removed += 1
+                        continue
+                    try:
+                        sections = item['sections']
+                    except:
+                        removed_courses_due_to_missing_section.append(item['course_number'])
+                        removed += 1
+                        continue
+                    for section in sections:
+                        if 'Instructor: TBA' not in section['professor'] and section['professor'] not in professors:
+                            sections.remove(section)
+                            removed_section += 1
+                    if sections:
+                        item['sections'] = sections
+                        final_data.append(item)
+                        written += 1
+                    else:
+                        removed_courses_due_to_missing_section.append(item['course_number'])
+                        removed += 1
+    return final_data, processed, removed, removed_section, written, removed_courses, removed_courses_due_to_missing_section
+
+courses_file = '/home/atajne/terpilot/data_scraping/intermediatedata/unique_courses_planetterp.json'
+professors_file = '/home/atajne/terpilot/data_scraping/intermediatedata/unique_professors_planetterp.json'
+data_dir = '/home/atajne/terpilot/data_scraping/testudo_data'
+
+courses_data = read_from_json(courses_file)
+professors_data = read_from_json(professors_file)
+
+final_courses = courses_data['final_list']
+final_professors = professors_data['final_list']
+
+final_data, processed, removed, removed_section, written, a, b = process_data(data_dir, final_courses, final_professors)
+
+print(f"Processed: {processed}")
+print(f"Removed: {removed}")
+print(f"Removed section: {removed_section}")
+print(f"Written: {written}")
+print(f"Removed courses: {a}")
+print(f"Removed courses due to missing section: {b}")
+
+write_to_json(final_data, 'final_testudo_data.json')
