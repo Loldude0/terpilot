a9e33806eacc9ae5d901f6480970bb8e4eeb7f58
Aryan Tajne <atajne@umd.edu>
2024-04-21 01:48:55 +0000

    changed name

--- databasecreation/create_tables.sql
+++ databasecreation/create_tables.sql
@@ -10,35 +10,35 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 
 GRANT ALL ON SCHEMA public TO postgres;
 
-create table course (
+CREATE TABLE course (
     course_id serial primary key,
-    course_name varchar(256) not null,
-    course_number varchar(12) not null,
-    course_credits int not null,
+    course_name varchar(256) NOT NULL,
+    course_number varchar(12) NOT NULL,
+    course_credits int NOT NULL,
     course_grading varchar(64),
     course_restriction varchar(256),
     course_description text,
     course_average_grade float,
     course_grading_chart text,
     course_prerequisites text,
-    course_corequisites text,
-)
+    course_corequisites text
+);
 
-create table professor (
+CREATE TABLE professor (
     professor_id serial primary key,
-    professor_name varchar(256) not null,
+    professor_name varchar(256) NOT NULL,
     professor_rating float,
     professor_summary text,
     professor_avg_gpa float,
-    professor_grading_chart text,
-)
+    professor_grading_chart text
+);
 
-create table section (
+CREATE TABLE section (
     section_id serial primary key,
-    course_name varchar(256) not null,
-    course_course foreign key references course(course_id),
-    professor foreign key references professor(professor_id),
+    course_name varchar(256) NOT NULL,
+    course_id int REFERENCES course(course_id),
+    professor_id int REFERENCES professor(professor_id),
     course_time varchar(256),
     course_seats varchar(256),
-    course_summary text,
-)
+    course_summary text
+);
